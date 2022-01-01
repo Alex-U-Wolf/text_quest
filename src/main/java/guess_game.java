e868c2f3efda3bd77f1314df6cadb224c1859c4c
Alex-U-Wolf <alexander.u.volkov@gmail.com>
2022-01-01 16:33:40 +0300

    RND game

---
+++ src/main/java/guess_game.java
@@ -0,0 +1,29 @@
+package src.main.java;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+public class guess_game {
+
+    int iter = 1;
+    int rnd = 0;
+    public void guess() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Random random = new Random();
+        rnd = random.nextInt(100);
+        System.out.println("Enter Integer: 0 - 100");
+        while (rnd!=iter) {
+            try {
+                iter = Integer.parseInt(br.readLine());
+            } catch (NumberFormatException nfe) {
+                System.err.println("Invalid Format!");
+            }
+            if (iter>rnd) System.out.println("Too much");
+            if (iter<rnd) System.out.println("Not enough");
+        }
+        System.out.println("Match! Value="+rnd);
+    }
+
+}
--- src/main/java/quest.java
+++ src/main/java/quest.java
@@ -1,7 +1,14 @@
 package src.main.java;
 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class quest {
-    public static void main(String[] args){
-        System.out.println("Quest test");
+
+    public static void main(String[] args) throws IOException {
+        guess_game gg = new guess_game();
+        gg.guess();
     }
 }
